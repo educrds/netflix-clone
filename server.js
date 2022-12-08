683f4759bd413b1084320af25fb8fbb2b847ac9f
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-12-08 20:12:01 -0300

    main route

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const path = require('path');
 
-let initialPath = path.join(__dirname, 'public')
+let initialPath = path.join(__dirname, 'public');
+
+let app = express();
+app.use(express.static(initialPath));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(initialPath, 'index.html'));
+});
