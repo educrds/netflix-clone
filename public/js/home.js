f9309895fa7f89bb95f9be0c64e07bcd0a4f61e8
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-12-08 21:39:03 -0300

    refatorado JS #1

--- public/js/home.js
+++ public/js/home.js
@@ -1,80 +1,67 @@
+const main = document.querySelector('.main');
 
-const main = document.querySelector(".main")
-
-fetch(genres_list_http + new URLSearchParams({
-  api_key: api_key
-}))
-.then(res => res.json())
-.then( data => {
-
-    data.genres.forEach(item => {
-      fetchMoviesListByGenres(item.id, item.name)
-    });
-})
-
-
-const fetchMoviesListByGenres = (id, genres)=>{
-  fetch(movie_genres_http + new URLSearchParams({
+fetch(
+  `${genres_list_http}${new URLSearchParams({
     api_key: api_key,
-    with_genres: id,
-    page: Math.floor(Math.random()*3) + 1
-  }))
-  .then(res => res.json())
-  .then(data =>{
-    makeCategoryElement(`${genres}_movies`, data.results)
-  })
-  .catch(err => console.log(err))
-}
+  })}`
+)
+  .then((res) => res.json())
+  .then((data) =>
+    data.genres.forEach(({ id, name }) => fetchMoviesListByGenres(id, name))
+  );
 
-const makeCategoryElement = (category, data)=>{
+const fetchMoviesListByGenres = (id, genres) => {
+  fetch(
+    `${movie_genres_http}${new URLSearchParams({
+      api_key: api_key,
+      with_genres: id,
+      page: Math.floor(Math.random() * 3) + 1,
+    })}`
+  )
+    .then((res) => res.json())
+    .then((data) => makeCategoryElement(`${genres}_movies`, data.results))
+    .catch((err) => console.log(err));
+};
+
+const makeCategoryElement = (category, data) => {
   main.innerHTML += `
   <div class="movie-list">
-
+  
     <button class="prev-btn">
-      <img src="img/prev.png" alt="previous button">
+        <img src="img/prev.png" alt="previous button">
     </button>
-
-    <h1 class="movie-category">${category.replace("_", " ")}</h1>
-    
-    <div class="movie-container" id="${category}">
-
-    </div>
-
+    <h1 class="movie-category">${category.replace('_', ' ')}</h1>
+    <div class="movie-container" id="${category}"></div>
     <button class="next-btn">
       <img src="img/next.png" alt="next button">
     </button>
-
+    
   </div>
   
-  `
-  makeCards(category, data)
-}
+  `;
+  makeCards(category, data);
+};
 
+const makeCards = (id, data) => {
+  const movieContainer = document.getElementById(id);
 
-const makeCards = (id, data)=>{
-  const movieContainer = document.getElementById(id)
+  data.forEach((item, i) => {
+    const { backdrop_path, poster_path, title } = item;
 
-  data.forEach((item, i)=>{
-    if(item.backdrop_path == null){
-      item.backdrop_path = item.poster_path;
-      if(item.backdrop_path == null){
-        return
+    if (backdrop_path == null) {
+      backdrop_path = poster_path;
+      if (backdrop_path == null) {
+        return;
       }
     }
 
-
     movieContainer.innerHTML += `
     <div class="movie">
-      <img src="${img_url}${item.backdrop_path}" alt="poster">
-      <p class="movie-title">${item.title}</p>
+      <img src="${img_url}${backdrop_path}" alt="poster">
+      <p class="movie-title">${title}</p>
     </div>
-    `
+    `;
 
-    if(i == data.length -1){
-      setTimeout(()=>{
-        setupScrooling()
-      }, 100)
-    }
-
-  })
-}
+    i == data.length - 1 ? setTimeout(() => setupScrooling(), 100) : null;
+  });
+};
